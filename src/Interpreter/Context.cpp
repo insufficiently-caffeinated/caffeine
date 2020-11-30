6aaa0b01096956a74b68b9982d97d91b52f652d0
Phantomical <phantom@lynches.ca>
2020-11-29 20:38:23 -0500

    Move solver and assertions into Context

--- src/Interpreter/Context.cpp
+++ src/Interpreter/Context.cpp
@@ -19,7 +19,8 @@ void assert_int(llvm::Type* type) {
   CAFFEINE_ABORT(message);
 }
 
-Context::Context(llvm::Function* function) {
+Context::Context(llvm::Function* function, std::shared_ptr<Solver> solver)
+    : solver_(std::move(solver)) {
   stack.emplace_back(function);
   StackFrame& frame = stack_top();
 
@@ -33,6 +34,24 @@ Context::Context(llvm::Function* function) {
 
 Context Context::fork() const {
   return Context{*this};
+}
+
+std::shared_ptr<Solver> Context::solver() const {
+  return solver_;
+}
+
+void Context::add(const Assertion& assertion) {
+  assertions_.push_back(assertion);
+}
+void Context::add(Assertion&& assertion) {
+  assertions_.push_back(std::move(assertion));
+}
+
+SolverResult Context::check(const Assertion& extra) {
+  return solver_->check(assertions_, extra);
+}
+std::unique_ptr<Model> Context::resolve(const Assertion& extra) {
+  return solver_->resolve(assertions_, extra);
 }
 
 } // namespace caffeine
