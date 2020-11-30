a76a50a5e4c3ac30774913793916b2c4f9fd44da
Misha Zharov <8266317+mishazharov@users.noreply.github.com>
2020-11-30 14:23:54 -0500

    Add Phi node and branch implementation (#30)
    This PR implements PHI nodes and branching in a similar way to decaf

--- src/Interpreter/Interpreter.cpp
+++ src/Interpreter/Interpreter.cpp
@@ -45,4 +45,60 @@ ExecutionResult Interpreter::visitAdd(llvm::BinaryOperator& op) {
   return ExecutionResult::Continue;
 }
 
+ExecutionResult Interpreter::visitPHINode(llvm::PHINode& node) {
+  auto& frame = ctx->stack_top();
+
+  // PHI nodes in the entry block is invalid.
+  CAFFEINE_ASSERT(frame.prev_block != nullptr);
+
+  auto value = frame.lookup(node.getIncomingValueForBlock(frame.prev_block));
+  frame.insert(&node, value);
+
+  return ExecutionResult::Continue;
+}
+
+ExecutionResult Interpreter::visitBranchInst(llvm::BranchInst& inst) {
+  if (!inst.isConditional()) {
+    ctx->stack_top().jump_to(inst.getSuccessor(0));
+    return ExecutionResult::Continue;
+  }
+
+  auto& frame = ctx->stack_top();
+  auto cond = frame.lookup(inst.getCondition());
+
+  auto zero = ConstantInt::Create(llvm::APInt(cond->type().bitwidth(), 0));
+  auto assertion = Assertion(cond);
+  auto is_t = ctx->check(assertion);
+  auto is_f = ctx->check(!assertion);
+
+  // Note: For the purposes of branching we consider unknown to be
+  //       equivalent to sat. Maybe future branches will bring the
+  //       equation back to being solvable.
+  if (is_t != SolverResult::UNSAT && is_f != SolverResult::UNSAT) {
+    auto fork = ctx->fork();
+
+    // In cases where both conditions are possible we follow the
+    // false path. This should be enough to get us out of most loops
+    // and actually exploring the rest of the program.
+    fork.add(assertion);
+    ctx->add(!assertion);
+
+    fork.stack_top().jump_to(inst.getSuccessor(0));
+    ctx->stack_top().jump_to(inst.getSuccessor(1));
+
+    queue->add_context(std::move(fork));
+    return ExecutionResult::Continue;
+  } else if (is_t != SolverResult::UNSAT) {
+    ctx->add(assertion);
+    ctx->stack_top().jump_to(inst.getSuccessor(0));
+    return ExecutionResult::Continue;
+  } else if (is_f != SolverResult::UNSAT) {
+    ctx->add(!assertion);
+    ctx->stack_top().jump_to(inst.getSuccessor(1));
+    return ExecutionResult::Continue;
+  } else {
+    return ExecutionResult::Stop;
+  }
+}
+
 } // namespace caffeine
