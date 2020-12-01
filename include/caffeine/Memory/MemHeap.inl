02cbc4c7173a44ec034ad62f4f8232614d818e58
Phantomical <phantom@lynches.ca>
2020-11-30 22:20:44 -0500

    Implement memory heap and pointers

--- include/caffeine/Memory/MemHeap.inl
+++ include/caffeine/Memory/MemHeap.inl
@@ -3,38 +3,52 @@
 
 #include "caffeine/Memory/MemHeap.h"
 
+#include <climits>
+
 namespace caffeine {
 
 /***************************************************
  * Allocation                                      *
  ***************************************************/
 
-const ref<Operation>& Allocation::size() const {
+inline const ref<Operation>& Allocation::size() const {
   return size_;
 }
-ref<Operation>& Allocation::size() {
+inline ref<Operation>& Allocation::size() {
   return size_;
 }
 
-const ref<Operation>& Allocation::data() const {
+inline const ref<Operation>& Allocation::data() const {
   return data_;
 }
-ref<Operation>& Allocation::data() {
+inline ref<Operation>& Allocation::data() {
   return data_;
 }
 
-const ref<Operation>& Allocation::address() const {
+inline const ref<Operation>& Allocation::address() const {
   return address_;
 }
-ref<Operation>& Allocation::address() {
+inline ref<Operation>& Allocation::address() {
   return address_;
 }
 
-bool Allocation::dead() const {
-  return dead_;
+/***************************************************
+ * Pointer                                         *
+ ***************************************************/
+
+inline AllocId Pointer::alloc() const {
+  return alloc_;
 }
-bool& Allocation::dead() {
-  return dead_;
+
+inline const ref<Operation>& Pointer::offset() const {
+  return offset_;
+}
+
+inline bool Pointer::is_resolved() const {
+  // TODO: This depends on some internal parts of slot_map which aren't really
+  //       meant to be exposed. It should be fine but if slotmap ever starts
+  //       using a different key type then it'll be necessary to rework this.
+  return alloc_.second == SIZE_MAX;
 }
 
 } // namespace caffeine
--- src/Memory/MemHeap.cpp
+++ src/Memory/MemHeap.cpp
@@ -1,8 +1,12 @@
 #include "caffeine/Memory/MemHeap.h"
 #include "caffeine/IR/Assertion.h"
+#include "caffeine/Interpreter/Context.h"
+#include "caffeine/Solver/Solver.h"
 #include "caffeine/Support/Assert.h"
 
 #include <llvm/ADT/SmallVector.h>
+
+#include <algorithm>
 
 namespace caffeine {
 
@@ -11,16 +15,15 @@ namespace caffeine {
  ***************************************************/
 
 Allocation::Allocation(const ref<Operation>& address,
-                       const ref<Operation>& size, const ref<Operation>& data,
-                       bool dead)
-    : address_(address), size_(size), data_(data), dead_(dead) {
+                       const ref<Operation>& size, const ref<Operation>& data)
+    : address_(address), size_(size), data_(data) {
   CAFFEINE_ASSERT(address->type().is_int());
   CAFFEINE_ASSERT(size->type().is_int());
   CAFFEINE_ASSERT(address->type().bitwidth() == size->type().bitwidth());
 }
 Allocation::Allocation(const ref<Operation>& address, const ConstantInt& size,
-                       const ref<Operation>& data, bool dead)
-    : Allocation(address, make_ref<Operation>(size), data, dead) {}
+                       const ref<Operation>& data)
+    : Allocation(address, make_ref<Operation>(size), data) {}
 
 void Allocation::overwrite(const ref<Operation>& newdata) {
   data_ = newdata;
@@ -135,6 +138,134 @@ void Allocation::write(const ref<Operation>& offset,
 
     overwrite(StoreOp::Create(data(), index, byte));
   }
+}
+
+/***************************************************
+ * Pointer                                         *
+ ***************************************************/
+
+Pointer::Pointer(const ref<Operation>& value)
+    : Pointer({SIZE_MAX, SIZE_MAX}, value) {}
+Pointer::Pointer(const AllocId& alloc, const ref<Operation>& offset)
+    : alloc_(alloc), offset_(offset) {}
+
+ref<Operation> Pointer::value(const MemHeap& heap) const {
+  if (is_resolved())
+    return BinaryOp::CreateAdd(heap[alloc()].address(), offset());
+  return offset();
+}
+
+Assertion Pointer::check_null(const MemHeap& heap) const {
+  return ICmpOp::CreateICmp(ICmpOpcode::EQ, value(heap), 0);
+}
+
+/***************************************************
+ * MemHeap                                         *
+ ***************************************************/
+
+Allocation& MemHeap::operator[](const AllocId& alloc) {
+  return allocs_.at(alloc);
+}
+const Allocation& MemHeap::operator[](const AllocId& alloc) const {
+  return allocs_.at(alloc);
+}
+
+AllocId MemHeap::allocate(const ref<Operation>& size,
+                          const ref<Operation>& alignment, Context& ctx) {
+  CAFFEINE_ASSERT(size->type() == alignment->type());
+  CAFFEINE_ASSERT(size->type().is_int());
+
+  auto allocation = Allocation(
+      Constant::Create(size->type(), ctx.next_constant()), size,
+      AllocOp::Create(size, ConstantInt::Create(llvm::APInt(8, 0xDC))));
+
+  // Ensure that the allocation is properly aligned
+  ctx.add(Assertion(ICmpOp::CreateICmp(
+      ICmpOpcode::EQ, BinaryOp::CreateURem(allocation.address(), alignment),
+      0)));
+
+  for (const auto& alloc : allocs_) {
+    /**
+     * Ensure that the new allocation doesn't overlap with any of the existing
+     * allocations.
+     */
+
+    auto new_start = allocation.address();
+    auto old_start = alloc.address();
+
+    auto new_end = BinaryOp::CreateAdd(allocation.address(), allocation.size());
+    auto old_end = BinaryOp::CreateAdd(alloc.address(), alloc.size());
+
+    auto cmp1 = ICmpOp::CreateICmp(ICmpOpcode::ULE, new_start, old_start);
+    auto cmp2 = ICmpOp::CreateICmp(ICmpOpcode::ULT, old_start, new_end);
+    auto cmp3 = ICmpOp::CreateICmp(ICmpOpcode::ULE, old_start, new_start);
+    auto cmp4 = ICmpOp::CreateICmp(ICmpOpcode::ULT, new_end, old_end);
+
+    ctx.add(!Assertion(BinaryOp::CreateOr(BinaryOp::CreateAnd(cmp1, cmp2),
+                                          BinaryOp::CreateAnd(cmp3, cmp4))));
+  }
+
+  return allocs_.insert(allocation);
+}
+
+void MemHeap::deallocate(const AllocId& alloc) {
+  auto value = allocs_.remove(alloc);
+
+  // Note: This likely means that we're trying to deallocate an allocation that
+  //       was created in an unrelated context.
+  CAFFEINE_ASSERT(value.has_value(),
+                  "tried to deallocate a nonexistant allocation");
+}
+
+bool MemHeap::check_live(const AllocId& alloc) const {
+  return allocs_.find(alloc) != allocs_.end();
+}
+
+Assertion MemHeap::check_valid(const Pointer& ptr) {
+  if (ptr.is_resolved()) {
+    return ICmpOp::CreateICmp(ICmpOpcode::ULE, ptr.offset(),
+                              (*this)[ptr.alloc()].size());
+  }
+
+  auto result = ConstantInt::Create(false);
+  auto value = ptr.value(*this);
+
+  for (const auto& alloc : allocs_) {
+    auto end = BinaryOp::CreateAdd(alloc.address(), alloc.size());
+    auto cmp1 = ICmpOp::CreateICmp(ICmpOpcode::ULE, alloc.address(), value);
+    auto cmp2 = ICmpOp::CreateICmp(ICmpOpcode::ULT, value, end);
+
+    // result |= (address <= value) && (value < address + size)
+    result = BinaryOp::CreateAnd(cmp1, cmp2);
+  }
+
+  return result;
+}
+
+llvm::SmallVector<Pointer, 1> MemHeap::resolve(const Pointer& ptr,
+                                               Context& ctx) const {
+  if (ptr.is_resolved())
+    return llvm::SmallVector<Pointer, 1>{ptr};
+
+  llvm::SmallVector<Pointer, 1> results;
+  auto value = ptr.value(*this);
+
+  auto end = allocs_.end();
+  for (auto it = allocs_.begin(); it != end; ++it) {
+    const auto& alloc = *it;
+
+    auto end = BinaryOp::CreateAdd(alloc.address(), alloc.size());
+    auto cmp1 = ICmpOp::CreateICmp(ICmpOpcode::ULE, alloc.address(), value);
+    auto cmp2 = ICmpOp::CreateICmp(ICmpOpcode::ULT, value, end);
+    auto assertion = BinaryOp::CreateAnd(cmp1, cmp2);
+
+    if (ctx.check(Assertion(assertion)) != SolverResult::UNSAT) {
+      results.push_back(
+          Pointer(it.key(), BinaryOp::CreateSub(value, alloc.address())));
+    }
+  }
+
+  return results;
 }
 
 } // namespace caffeine
