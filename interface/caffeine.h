9f18da0f431446a47b277981277adf176f19f2db
Phantomical <phantom@lynches.ca>
2020-12-02 23:09:46 -0500

    Add caffeine interface header

---
+++ interface/caffeine.h
@@ -0,0 +1,33 @@
+#ifndef CAFFEINE_INTERFACE_H
+#define CAFFEINE_INTERFACE_H
+
+#pragma once
+
+#include <stdbool.h>
+
+#ifndef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Assert that the condition is true.
+ *
+ * In cases where the symbolic executor determines that the
+ * condition could be false, it will produce a test case with
+ * concrete inputs which reproduce the failure.
+ */
+void caffeine_assert(bool cond);
+
+/**
+ * Assume that a condition is true.
+ *
+ * This will silently remove any executions in which the condition
+ * could evaluate to false.
+ */
+void caffeine_assume(bool cond);
+
+#ifndef __cplusplus
+}
+#endif
+
+#endif
