a564295baa8e9c7bfdccd2bd255dc534bbf3473a
Phantomical <phantom@lynches.ca>
2020-12-03 15:22:58 -0500

    Allow showing stacktraces when tests fail under certain configurations

--- test/unit/main.cpp
+++ test/unit/main.cpp
@@ -1,7 +1,10 @@
 
 #include <gtest/gtest.h>
 
+#include <llvm/Support/InitLLVM.h>
+
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
+  llvm::InitLLVM X(argc, argv);
   return RUN_ALL_TESTS();
 }
