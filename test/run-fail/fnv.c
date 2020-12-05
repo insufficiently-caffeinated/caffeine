abe145d2c8056dd4deb4026a5a4678442fb68646
Misha Zharov <mishazharov1@gmail.com>
2020-12-05 17:43:27 -0500

    Add fnv

---
+++ test/run-fail/fnv.c
@@ -0,0 +1,19 @@
+#include "caffeine.h"
+#include <stdint.h>
+
+#define FNV_PRIME \
+  UINT64_C(1099511628211)
+
+#define FNV_OFFSET \
+  UINT64_C(14695981039346656037)
+
+uint64_t fnv(uint64_t x) {
+  uint64_t hash = FNV_OFFSET;
+  hash ^= x;
+  hash *= FNV_PRIME;
+  return hash;
+}
+
+void test(uint64_t x) {
+  caffeine_assert(fnv(x) != 0);
+}
