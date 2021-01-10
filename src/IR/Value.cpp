f66983269b303c906c66d431d290e23126872813
Sean Lynch <phantom@lynches.ca>
2021-01-09 20:46:09 -0500

    Add support for byte array Value instances (#94)
    * Add support for array Value instances
    * Implement load and store operations for Value

--- src/IR/Value.cpp
+++ src/IR/Value.cpp
@@ -13,11 +13,27 @@ using llvm::APInt;
 
 namespace caffeine {
 
+Value::ArrayData::ArrayData(const SharedArray& data, uint32_t idx_width)
+    : data(data), index_bitwidth(idx_width) {}
+Value::ArrayData::ArrayData(SharedArray&& data, uint32_t idx_width)
+    : data(data), index_bitwidth(idx_width) {}
+
 Value::Value(const APInt& apint) : inner_(apint) {}
 Value::Value(APInt&& apint) : inner_(std::move(apint)) {}
 
 Value::Value(const APFloat& apfloat) : inner_(apfloat) {}
 Value::Value(APFloat&& apfloat) : inner_(std::move(apfloat)) {}
+
+Value::Value(const SharedArray& array, Type index_ty)
+    : inner_([&]() -> ArrayData {
+        CAFFEINE_ASSERT(index_ty.is_int(), "index type was not an integer");
+        return ArrayData(array, index_ty.bitwidth());
+      }()) {}
+Value::Value(SharedArray&& array, Type index_ty)
+    : inner_([&]() -> ArrayData {
+        CAFFEINE_ASSERT(index_ty.is_int(), "index type was not an integer");
+        return ArrayData(std::move(array), index_ty.bitwidth());
+      }()) {}
 
 Value::Value(const ConstantInt& constant) : Value(constant.value()) {}
 Value::Value(const ConstantFloat& constant) : Value(constant.value()) {}
@@ -30,6 +46,8 @@ Type Value::type() const {
     return Type::type_of(apint());
   case Float:
     return Type::type_of(apfloat());
+  case Array:
+    return Type::array_ty(std::get<ArrayData>(inner_).index_bitwidth);
   }
 
   CAFFEINE_UNREACHABLE();
@@ -53,6 +71,15 @@ const APFloat& Value::apfloat() const {
   return std::get<llvm::APFloat>(inner_);
 }
 
+SharedArray& Value::array() {
+  CAFFEINE_ASSERT(is_array());
+  return std::get<ArrayData>(inner_).data;
+}
+const SharedArray& Value::array() const {
+  CAFFEINE_ASSERT(is_array());
+  return std::get<ArrayData>(inner_).data;
+}
+
 bool Value::operator==(const Value& v) const {
   if (kind() != v.kind())
     return false;
@@ -64,6 +91,8 @@ bool Value::operator==(const Value& v) const {
     return apint() == v.apint();
   case Float:
     return apfloat().bitwiseIsEqual(v.apfloat());
+  case Array:
+    return array() == v.array();
   }
 
   CAFFEINE_UNREACHABLE();
@@ -190,6 +219,30 @@ Value Value::bitcast(const Value& v, const Type& tgt) {
 
     return v.apfloat().bitcastToAPInt();
   }
+}
+
+Value Value::load(const Value& data, const Value& index) {
+  CAFFEINE_ASSERT(data.is_array());
+  CAFFEINE_ASSERT(index.is_int());
+  CAFFEINE_ASSERT(data.type().bitwidth() == index.type().bitwidth());
+  CAFFEINE_ASSERT(index.apint().ult(data.array().size()),
+                  "attempted to load from out of bounds index");
+
+  return Value(
+      llvm::APInt(8, (uint8_t)data.array()[index.apint().getLimitedValue()]));
+}
+Value Value::store(const Value& data, const Value& index, const Value& byte) {
+  CAFFEINE_ASSERT(data.is_array());
+  CAFFEINE_ASSERT(index.is_int());
+  CAFFEINE_ASSERT(data.type().bitwidth() == index.type().bitwidth());
+  CAFFEINE_ASSERT(byte.type() == Type::int_ty(8));
+  CAFFEINE_ASSERT(index.apint().ult(data.array().size()),
+                  "attempted to load from out of bounds index");
+
+  auto array = data.array();
+  array.store(index.apint().getLimitedValue(),
+              (char)byte.apint().getLimitedValue());
+  return Value(std::move(array), index.type());
 }
 
 std::ostream& operator<<(std::ostream& os, const Value& v) {
