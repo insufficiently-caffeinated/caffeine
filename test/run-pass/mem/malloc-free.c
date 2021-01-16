a009769ce713c9c055be845f900f83c82c887524
Phantomical <phantom@lynches.ca>
2021-01-16 17:42:43 -0500

    Add test cases for malloc and free

---
+++ test/run-pass/mem/malloc-free.c
@@ -0,0 +1,20 @@
+// SKIP TEST
+
+#include "caffeine.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+void test(size_t x) {
+  caffeine_assume(x < 32);
+  caffeine_assume(x >= 1);
+
+  uint32_t* mem = malloc(x * sizeof(uint32_t));
+  
+  for (uint32_t i = 0; i < x; ++i) {
+    mem[i] = i * 2;
+  }
+
+  for (uint32_t i = 0; i < x; ++i) {
+    caffeine_assert(mem[i] == i * 2);
+  }
+}
