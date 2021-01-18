4dcca0c30d82eafbf69b71b93734b32c3857ecf5
Phantomical <phantom@lynches.ca>
2021-01-18 00:47:29 -0500

    Make malloc-free test faster to execute

--- test/run-pass/mem/malloc-free.c
+++ test/run-pass/mem/malloc-free.c
@@ -1,17 +1,21 @@
-// SKIP TEST
 
 #include "caffeine.h"
 #include <stdint.h>
 #include <stdlib.h>
 
+// Needed to avoid vector load/stores
+__attribute__((noinline)) uint32_t mul2(uint32_t x) {
+  return x * 2;
+}
+
 void test(size_t x) {
-  caffeine_assume(x < 32);
+  caffeine_assume(x < 4);
   caffeine_assume(x >= 1);
 
   uint32_t* mem = malloc(x * sizeof(uint32_t));
 
   for (uint32_t i = 0; i < x; ++i) {
-    mem[i] = i * 2;
+    mem[i] = mul2(i);
   }
 
   for (uint32_t i = 0; i < x; ++i) {
