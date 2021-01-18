3462aec8e138609ba113f6b9a2f617224cc46f10
Phantomical <phantom@lynches.ca>
2021-01-17 23:26:22 -0500

    Fix warning in mem/select-array test

--- test/run-fail/mem/select-array.c
+++ test/run-fail/mem/select-array.c
@@ -5,7 +5,7 @@
 int32_t array1[] = {1};
 int32_t array2[] = {2};
 
-void test(int32_t x, int32_t y) {
+void test(int32_t x) {
   int32_t* mem = x == 0 ? array1 : array2;
 
   caffeine_assert(*mem == 2);
