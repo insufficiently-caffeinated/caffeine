a4ca31233df7bb7aaf9570213a03a8f9dd815f30
Phantomical <phantom@lynches.ca>
2021-02-03 19:59:07 -0500

    Rewrite DecompSolver to use decompose transform

--- src/Solver/DecompSolver.cpp
+++ src/Solver/DecompSolver.cpp
@@ -1,7 +1,7 @@
 
 #include "caffeine/Solver/DecompSolver.h"
 #include "caffeine/IR/Assertion.h"
-#include "caffeine/IR/Matching.h"
+#include "caffeine/IR/Transforms.h"
 #include "caffeine/Solver/SequenceSolver.h"
 
 #include <algorithm>
@@ -9,39 +9,12 @@
 namespace caffeine {
 
 void DecompSolver::transform(std::vector<Assertion>& assertions) {
-  using std::swap;
-  using namespace matching;
-
+  transforms::decompose(assertions);
+  
   auto it = std::remove_if(
       assertions.begin(), assertions.end(),
       [](const auto& assertion) { return assertion.is_constant_value(true); });
   assertions.erase(it, assertions.end());
-
-  for (size_t i = 0; i < assertions.size(); ++i) {
-    // Keep performing the transformations until they stop working
-    while (true) {
-      ref<Operation> lhs, rhs, value;
-
-      if (matches(assertions[i], And(lhs, rhs))) {
-        *assertions[i].value() = *lhs;
-        assertions.emplace_back(std::move(rhs));
-        continue;
-      }
-
-      if (matches(assertions[i], Not(Or(lhs, rhs)))) {
-        *assertions[i].value() = *UnaryOp::CreateNot(lhs);
-        assertions.emplace_back(UnaryOp::CreateNot(rhs));
-        continue;
-      }
-
-      if (matches(assertions[i], Not(Not(value)))) {
-        *assertions[i].value() = *value;
-        continue;
-      }
-
-      break;
-    }
-  }
 }
 
 SolverResult DecompSolver::check(std::vector<Assertion>& assertions,
