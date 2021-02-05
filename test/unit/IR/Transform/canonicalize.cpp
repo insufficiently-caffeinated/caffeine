bef508568a52d658ed6430842e60555ae9d51f18
Phantomical <phantom@lynches.ca>
2021-02-05 00:59:46 -0500

    Implement canonicalize transform

---
+++ test/unit/IR/Transform/canonicalize.cpp
@@ -0,0 +1,20 @@
+
+#include "caffeine/IR/Assertion.h"
+#include "caffeine/IR/Transforms.h"
+
+#include <gtest/gtest.h>
+
+using namespace caffeine;
+using namespace caffeine::transforms;
+
+TEST(CanonicalizeTransformTests, canonicalFalseAssertions) {
+  std::vector<Assertion> assertions = {
+      Assertion(ConstantInt::Create(false)),
+      Assertion(ConstantInt::Create(true)),
+      Assertion(Constant::Create(Type::int_ty(1), 0))};
+
+  canonicalize(assertions);
+
+  ASSERT_EQ(assertions.size(), 1);
+  ASSERT_TRUE(assertions[0].is_constant_value(false));
+}
