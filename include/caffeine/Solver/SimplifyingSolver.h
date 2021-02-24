99c0ce1153b89906b51d1f655732d630c3084283
Phantomical <phantom@lynches.ca>
2021-02-24 00:12:12 -0500

    Implement simplifying solver

---
+++ include/caffeine/Solver/SimplifyingSolver.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "caffeine/Solver/Solver.h"
+
+namespace caffeine {
+
+/**
+ * Solver that attempts to convert expressions to a canonical form.
+ *
+ * What exactly the canonical form for expressions are hasn't been defined yet
+ * but currently there are two guarantees from this solver:
+ *  - There will be no unevaluated constant subexpressions (e.g. 3 + 4)
+ *  - There will be no repeated duplicate assertions
+ *
+ * Always returns unknown.
+ */
+class SimplifyingSolver final : public Solver {
+public:
+  SimplifyingSolver() = default;
+
+  SolverResult check(std::vector<Assertion>& assertions,
+                     const Assertion& extra) override;
+
+  std::unique_ptr<Model> resolve(std::vector<Assertion>& assertions,
+                                 const Assertion& extra) override;
+};
+
+} // namespace caffeine
