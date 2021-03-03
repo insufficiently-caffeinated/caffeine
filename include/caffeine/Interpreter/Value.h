bce14e3e730ee990ac051ac9130bf634f444fe37
Phantomical <phantom@lynches.ca>
2021-03-03 00:29:27 -0500

    Redesign value representation in context

--- include/caffeine/Interpreter/Value.h
+++ include/caffeine/Interpreter/Value.h
@@ -13,6 +13,90 @@
 namespace caffeine {
 
 /**
+ * A LLVM value that is not an aggregate or a vector.
+ *
+ * Currently, this can be either
+ * - a scalar expression, or
+ * - a pointer
+ */
+class LLVMScalar {
+public:
+  enum Kind { Expr, Pointer };
+
+private:
+  std::variant<OpRef, caffeine::Pointer> inner_;
+
+public:
+  LLVMScalar(const OpRef& value);
+  LLVMScalar(const caffeine::Pointer& value);
+
+  Kind kind() const;
+
+  bool is_expr() const;
+  bool is_pointer() const;
+
+  const OpRef& expr() const;
+  const caffeine::Pointer& pointer() const;
+};
+
+/**
+ * A general LLVM value.
+ *
+ * This can be any one of:
+ * - A scalar or pointer, or
+ * - A vector of scalars or pointers, or
+ * - An aggregate structure containing nested LLVMValues
+ *
+ * In terms of implementation, this is implemented by having a vector of either
+ * LLVMValues (for aggregates) and LLVMScalars (for vectors and scalars). A
+ * scalar is logically a vector of size 1 and this class follows that
+ * convention.
+ */
+class LLVMValue {
+public:
+  using OpVector = llvm::SmallVector<LLVMScalar, 4>;
+
+private:
+  enum { Vector, Aggregate };
+
+  std::variant<OpVector, std::vector<LLVMValue>> inner_;
+
+public:
+  LLVMValue(const LLVMScalar& value);
+  LLVMValue(OpVector&& values);
+  LLVMValue(llvm::ArrayRef<LLVMScalar> values);
+  LLVMValue(llvm::ArrayRef<LLVMValue> values);
+  LLVMValue(std::vector<LLVMValue>&& values);
+
+  bool is_scalar() const;
+  bool is_vector() const;
+  bool is_aggregate() const;
+
+  const LLVMScalar& scalar() const;
+  llvm::ArrayRef<LLVMScalar> vector() const;
+  llvm::ArrayRef<LLVMValue> aggregate() const;
+
+  size_t num_elements() const;
+  const LLVMScalar& element(size_t idx) const;
+  llvm::ArrayRef<LLVMScalar> elements() const;
+
+  size_t num_members() const;
+  const LLVMValue& member(size_t idx) const;
+  llvm::ArrayRef<LLVMValue> members() const;
+};
+
+/**
+ * Map the elements composing any number of vectors to a new vector with the
+ * same length. For this to work all vectors must have the same length otherwise
+ * an assertion will fire.
+ *
+ * This is generally meant to match the semantics needed when implementing LLVM
+ * opcodes.
+ */
+template <typename F, typename... Vals>
+LLVMValue transform_elements(F&& func, const Vals&... values);
+
+/**
  * An LLVM value as represented within a stack frame.
  *
  * Can be either
