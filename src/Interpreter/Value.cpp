237da502b203e644291056440e1c4439f68ff23e
Phantomical <phantom@lynches.ca>
2021-03-10 20:05:29 -0500

    Implement ostream overloads for LLVMValue and LLVMScalar

--- src/Interpreter/Value.cpp
+++ src/Interpreter/Value.cpp
@@ -137,4 +137,52 @@ std::ostream& operator<<(std::ostream& os, const ContextValue& value) {
   return os << ">";
 }
 
+std::ostream& operator<<(std::ostream& os, const LLVMScalar& value) {
+  if (value.is_pointer()) {
+    const Pointer& ptr = value.pointer();
+
+    if (ptr.is_resolved())
+      return os << "[" << ptr.alloc().first << " offset " << *ptr.offset()
+                << "]";
+    return os << "[" << *ptr.offset() << "]";
+  }
+
+  return os << *value.expr();
+}
+
+std::ostream& operator<<(std::ostream& os, const LLVMValue& value) {
+  if (value.is_scalar())
+    return os << value.scalar();
+
+  if (value.is_vector()) {
+    os << "<";
+    bool is_first = true;
+
+    for (const auto& val : value.elements()) {
+      if (is_first)
+        is_first = false;
+      else
+        os << ",\n ";
+
+      os << val;
+    }
+
+    return os << ">";
+  } else {
+    os << "{";
+    bool is_first = true;
+
+    for (const auto& val : value.members()) {
+      if (is_first)
+        is_first = false;
+      else
+        os << ",\n ";
+
+      os << val;
+    }
+
+    return os << "}";
+  }
+}
+
 } // namespace caffeine
