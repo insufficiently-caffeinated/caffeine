ff0ffc7f468944ffdc3ea7afb37be99a85ec90b7
Phantomical <phantom@lynches.ca>
2021-03-10 19:43:08 -0500

    Silence warning

--- src/IR/Debug.cpp
+++ src/IR/Debug.cpp
@@ -3,8 +3,16 @@
 #include <iostream>
 
 namespace caffeine {
+
+namespace {
+  // Separate method to silence warnings
+  bool is_nonnull(const void* x) {
+    return x;
+  }
+} // namespace
+
 void Operation::DebugPrint() const {
-  if ((void*)this) {
+  if (is_nonnull(this)) {
     std::cout << *this << std::endl;
   } else {
     std::cout << "NULL" << std::endl;
