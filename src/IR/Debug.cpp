f01b91b047c9b03fb6902a0e4ee131229f2b33f3
Sean Lynch <phantom@lynches.ca>
2021-03-10 22:02:27 -0500

    Handle the case where this is null in DebugPrint (#236)
    * Handle the case where this is null in DebugPrint
    
    This is UB in C++ but it's more useful to have DebugPrint print NULL
    when calling it in the debugger than causing a segfault in the middle of
    your debug session.
    
    * Silence warning

--- src/IR/Debug.cpp
+++ src/IR/Debug.cpp
@@ -3,7 +3,19 @@
 #include <iostream>
 
 namespace caffeine {
+
+namespace {
+  // Separate method to silence warnings
+  bool is_nonnull(const void* x) {
+    return x;
+  }
+} // namespace
+
 void Operation::DebugPrint() const {
-  std::cout << *this << std::endl;
+  if (is_nonnull(this)) {
+    std::cout << *this << std::endl;
+  } else {
+    std::cout << "NULL" << std::endl;
+  }
 }
 } // namespace caffeine
