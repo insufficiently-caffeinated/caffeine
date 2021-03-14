01e7ec639db7bf8f2665db84298cfec5430930b7
Phantomical <phantom@lynches.ca>
2021-03-14 13:19:06 -0400

    Fix compilation errors

--- src/Interpreter/ExprEval.cpp
+++ src/Interpreter/ExprEval.cpp
@@ -447,7 +447,6 @@ LLVMValue ExprEvaluator::visitFNeg(llvm::UnaryOperator& op) {
 }
 
 LLVMValue ExprEvaluator::visitPtrToInt(llvm::PtrToIntInst& inst) {
-  const auto& layout = ctx->mod->getDataLayout();
   return transform_elements(
       [&](const LLVMScalar& value) {
         return LLVMScalar(UnaryOp::CreateTruncOrZExt(
