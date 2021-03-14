e14648775020485941c0300326ab94497cb3476e
Sean Lynch <phantom@lynches.ca>
2021-03-14 11:56:49 -0400

    [3/N] Interpreter redesign - Introduce simple expression evaluator (#230)
    * Redesign value representation in context
    
    * Add conversions between LLVMValue and ContextValue
    
    * Make LLVMValue constructors explicit
    
    * Implement LLVM expression evaluator
    
    Currently, this is a visitor that can evaluate a number of pure
    arithmetic operations.
    
    * Add extra visit overload
    
    * Implement methods of the Unevaluatable exception
    
    * Add try_visit method
    
    * Implement FPToSI and FPToUI visit methods
    
    * Implement visitors for global data in ExprEvaluator
    
    * Make ExprEvaluator a friend of StackFrame
    
    * Formatting

---
+++ include/caffeine/Interpreter/ExprEval.h
@@ -0,0 +1,98 @@
+#pragma once
+
+#include "caffeine/IR/Operation.h"
+#include <llvm/IR/InstVisitor.h>
+#include <optional>
+#include <stdexcept>
+
+namespace caffeine {
+
+class Context;
+class LLVMValue;
+
+/**
+ * LLVM IR expression evaluator.
+ *
+ * This class is designed to evaluate expressions which don't require any
+ * context forking. This makes it suitable for evaluating LLVM constant
+ * expressions and certain simple instructions.
+ *
+ * This class will never insert a value into the context. Depending on
+ * configuration, it may create new allocations if attempting to evaluate a
+ * global allocation.
+ */
+class ExprEvaluator : public llvm::InstVisitor<ExprEvaluator, LLVMValue> {
+private:
+  using BaseType = llvm::InstVisitor<ExprEvaluator, LLVMValue>;
+
+public:
+  struct Options {
+    bool create_allocations = true;
+
+    constexpr Options() noexcept {}
+  };
+
+  class Unevaluatable : public std::exception {
+  private:
+    llvm::Value* expr_;
+    const char* context_;
+    mutable std::string msg_cache_;
+
+  public:
+    explicit Unevaluatable(llvm::Value* expr, const char* context = nullptr);
+
+    const char* what() const throw() override;
+    llvm::Value* expr() const;
+  };
+
+  explicit ExprEvaluator(Context* ctx, Options options = Options());
+
+  LLVMValue visit(llvm::Value* val);
+  LLVMValue visit(llvm::Value& val);
+
+  std::optional<LLVMValue> try_visit(llvm::Value* val);
+
+  /*********************************************
+   * InstVisitor override methods              *
+   *********************************************/
+
+  LLVMValue visitInstruction(llvm::Instruction& inst);
+
+  LLVMValue visitAdd(llvm::BinaryOperator& op);
+  LLVMValue visitSub(llvm::BinaryOperator& op);
+  LLVMValue visitMul(llvm::BinaryOperator& op);
+  LLVMValue visitUDiv(llvm::BinaryOperator& op);
+  LLVMValue visitSDiv(llvm::BinaryOperator& op);
+  LLVMValue visitURem(llvm::BinaryOperator& op);
+  LLVMValue visitSRem(llvm::BinaryOperator& op);
+
+  LLVMValue visitShl(llvm::BinaryOperator& op);
+  LLVMValue visitLShr(llvm::BinaryOperator& op);
+  LLVMValue visitAShr(llvm::BinaryOperator& op);
+  LLVMValue visitAnd(llvm::BinaryOperator& op);
+  LLVMValue visitOr(llvm::BinaryOperator& op);
+  LLVMValue visitXor(llvm::BinaryOperator& op);
+
+  LLVMValue visitFAdd(llvm::BinaryOperator& op);
+  LLVMValue visitFSub(llvm::BinaryOperator& op);
+  LLVMValue visitFMul(llvm::BinaryOperator& op);
+  LLVMValue visitFDiv(llvm::BinaryOperator& op);
+
+  LLVMValue visitFNeg(llvm::UnaryOperator& op);
+
+  LLVMValue visitTrunc(llvm::CastInst& op);
+  LLVMValue visitSExt(llvm::CastInst& op);
+  LLVMValue visitZExt(llvm::CastInst& op);
+  LLVMValue visitFPTrunc(llvm::CastInst& op);
+  LLVMValue visitFPExt(llvm::CastInst& op);
+  LLVMValue visitUIToFP(llvm::CastInst& op);
+  LLVMValue visitSIToFP(llvm::CastInst& op);
+  LLVMValue visitFPToUI(llvm::CastInst& op);
+  LLVMValue visitFPToSI(llvm::CastInst& op);
+
+private:
+  Context* ctx;
+  Options options;
+};
+
+} // namespace caffeine
