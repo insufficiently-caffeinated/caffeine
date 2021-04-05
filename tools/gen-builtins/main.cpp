3b68fbf0d843d225ac73a4550c2465301f34a3bf
Sean Lynch <phantom@lynches.ca>
2021-04-05 00:12:34 -0400

    Introduce tool to generate implementations of LLVM builtins (#294)
    The current state of the repo (as of this commit) is that we have
    builtin implementations for the most common calls to the llvm.memset.*
    intrinsic. Namely, we really only support the
      void llvm.memset.p0i8.i64(i8* %dst, i8 %val, i64 %len, i1 %isvolatile)
    variant. This is done by hand-implementing a function
      void caffeine_builtin_memset(...)
    with the same signature in _LLVM IR_ and then having the interpreter
    patch calls to the intrinsic to instead point to the function we
    implemented by hand.
    
    This has some big limitations:
    1. We don't support anything other than the most basic memset calls.
       Anything that gets generated to use a different type will fail to
       compile. As an example, this will fail on platforms where pointers
       are 32-bits in size.
    2. We don't support any platform that is not data-layout-compatible with
       x86_64-linux-gnu. This is the more relevant issue because it means
       that we cannot compile our test cases for windows.
    
    Both limitations are decently large problems. The solution I've
    undertaken here is to instead generate the required builtin
    implementations at build time. This means that we never end up needing
    an unsupported function variant since we support _all_ the variants.
    
    As a bonus, we can now also use IR names that can't be defined from C.

---
+++ tools/gen-builtins/main.cpp
@@ -0,0 +1,68 @@
+
+#include "builtins.h"
+#include "caffeine/Support/Assert.h"
+#include <fmt/format.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/InitLLVM.h>
+#include <llvm/Support/SourceMgr.h>
+#include <llvm/Support/WithColor.h>
+
+using namespace llvm;
+
+cl::opt<std::string> InputFilename(cl::Positional, cl::Required,
+                                   cl::desc("<input file>"));
+cl::opt<std::string> OutputFilename("o", cl::desc("output filename"),
+                                    cl::value_desc("filename"), cl::init("-"));
+
+int main(int argc, char** argv) {
+  llvm::InitLLVM X{argc, argv};
+  CAFFEINE_ASSERT(argc != 0,
+                  "gen-builtins called with 0 command line arguments");
+
+  llvm::cl::ParseCommandLineOptions(argc, argv,
+                                    "\n  Replace all calls to certain LLVM "
+                                    "builtins with implementations that"
+                                    "\n  caffeine can execute.");
+
+  llvm::LLVMContext context;
+  llvm::SMDiagnostic error;
+
+  std::unique_ptr<llvm::Module> module =
+      llvm::parseIRFile(InputFilename, error, context);
+  if (!module) {
+    errs() << argv[0] << ": ";
+    WithColor::error() << " loading file '" << InputFilename << "'\n";
+    return EXIT_FAILURE;
+  }
+
+  for (llvm::Function& function : module->functions()) {
+    if (!function.isDeclaration())
+      continue;
+
+    auto name = function.getName();
+    if (name.startswith("llvm.memset.")) {
+      caffeine::generateMemset(module.get(), &function);
+      continue;
+    }
+  }
+
+  // NOTE: WriteBitcodeToFile seems to produce something that llvm-dis can't
+  // parse for LLVM 10.0.0. To work around that I'm printing using textual IR.
+  if (OutputFilename == "-") {
+    module->print(llvm::outs(), nullptr);
+  } else {
+    std::error_code EC;
+    llvm::raw_fd_ostream os(OutputFilename, EC);
+
+    if (EC) {
+      errs() << argv[0] << ": ";
+      WithColor::error() << " unable to write to output file '"
+                         << OutputFilename << "': " << EC.message() << "\n";
+      return 1;
+    }
+
+    module->print(os, nullptr);
+  }
+}
