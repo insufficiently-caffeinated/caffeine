efe8ed590b38e479b233bc38070f63f6c56e2283
Sean Lynch <phantom@lynches.ca>
2021-04-07 00:36:00 -0400

    Rewrite deps handling to not include dependency as a subdirectory (#305)
    At the moment, FetchContent will always add the fetched depenency as a
    subdirectory. This works ok if the dependency's CMakeLists is written to
    work properly when added as a subdirectory. Unfortunately, the CMake
    configurations for most projects are... not. This limits the number of
    dependencies that we can bundle using FetchContent to only a few.
    
    However, cmake does already have support for doing all the difficult
    bits of bundling dependencies by using ExternalProject_Add.
    Unfortunately this doesn't help us directly as ExternalProject_Add is
    targetted towards building a super-project that assembles multiple
    subprojects into one big build.
    
    This can be worked around by generating a simple cmake project that does
    nothing but call ExternalProject_Add. This is also effectively what the
    FetchContent module does except that it disables all the steps except
    the download one and expects you to use add_subdirectory.
    
    So to include a dependency we do the following:
    1. First, check to see if we can find the project via find_package. If
       so then we're done.
    2. Generate a CMakeLists.txt in a project-specific subdirectory of the
       _deps folder in the binary dir. We pick the name so as not to
       conflict with the directory names used by FetchContent.
    3. Configure the subproject to install to a subdirectory within _deps.
       I've chosen to use install for this. If we add a project named
       install as a depedency then we'll have to change this. I don't think
       this will be an issue.
    4. Build the project (during caffeine configuration).
    5. And finally, use find_package to find the newly installed files
       within the local installation directory.
    
    Once the project is built we'll stop after step 1 since we find the
    installed files from the last time we ran configure.
    
    Note that there is some extra work that happens to cover these cases:
    - Multi-configuration generators (e.g. Visual Studio, XCode): To make
      these work we need to build the subproject for all supported
      configurations.
    - Updating the config file: Need to always rebuild the subproject if the
      config has changed.

--- cmake/CaffeineDependencies.cmake
+++ cmake/CaffeineDependencies.cmake
@@ -1,19 +1,108 @@
 
-if (NOT magic_enum_NO_PACKAGE_FOUND)
-  find_package(magic_enum 0.7.2 QUIET)
-endif()
+include(FetchContent)
 
-if (NOT magic_enum_FOUND)
-  include(FetchContent)
+set(CAFFEINE_DEP_INSTALL_DIR "${CMAKE_BINARY_DIR}/_deps/install")
+set(CAFFEINE_DEP_CONFIG_IN "${CMAKE_CURRENT_LIST_DIR}/CaffeineTemplateCMakeLists.txt.in")
+list(APPEND CMAKE_PREFIX_PATH "${CAFFEINE_DEP_INSTALL_DIR}")
 
-  FetchContent_Declare(
-    magic_enum
-    GIT_REPOSITORY https://github.com/Neargye/magic_enum.git
-    GIT_TAG        v0.7.2
-    GIT_SHALLOW TRUE
+# Add a new dependency, or build it if it cannot be found on the local system.
+#
+# The first two arguments are the package name and the version that we need to
+# find. These will be used for find_package. Any remaining arguments are passed
+# through to ExternalProject_Add.
+#
+# How it Works
+# ============
+# The main idea is that, at configuration time, we use ExternalProject_Add in a
+# separate cmake project to build and install the dependency to a local directory.
+# From there, we can use find_package with the custom install directory added to
+# the search path to find the dependencies.
+#
+# Now that that's happened the first time around, the next time find_package will
+# immediately find the installed files and we can skip having to build anything.
+function(caffeine_dependency PACKAGE VERSION)
+  if (NOT $CACHE{${PACKAGE}_BUILT_LOCALLY})
+    find_package(${PACKAGE} ${VERSION} QUIET)
+    if (${PACKAGE}_FOUND)
+      return()
+    endif()
+  endif()
+
+  set(${PACKAGE}_BUILT_LOCALLY TRUE CACHE INTERNAL "")
+  set(SUB_DIR "${CMAKE_BINARY_DIR}/_deps/${PACKAGE}/populate")
+  set(PREFIX  "${CMAKE_BINARY_DIR}/_deps/${PACKAGE}")
+
+  make_directory("${SUB_DIR}")
+
+  set(QUOTED_ARGS "")
+  foreach(ARG ${ARGN})
+    set(QUOTED_ARGS "${QUOTED_ARGS} \"${ARG}\"")
+  endforeach()
+
+  set(PACKAGE_INSTALL_PREFIX "${CAFFEINE_DEP_INSTALL_DIR}")
+
+  if (NOT "${CMAKE_BUILD_TYPE}" STREQUAL "")
+    set(PACKAGE_CONFIGURATION_TYPES "${CMAKE_BUILD_TYPE}")
+  else()
+    set(PACKAGE_CONFIGURATION_TYPES "${CMAKE_CONFIGURATION_TYPES}")
+  endif()
+
+  configure_file("${CAFFEINE_DEP_CONFIG_IN}" "${SUB_DIR}/CMakeLists.copy.txt" @ONLY)
+
+  set(NEEDS_UPDATE FALSE)
+  if (NOT EXISTS "${SUB_DIR}/CMakeLists.txt")
+    set(NEEDS_UPDATE TRUE)
+  else()
+    file(SHA512 "${SUB_DIR}/CMakeLists.txt"      OLD_HASH)
+    file(SHA512 "${SUB_DIR}/CMakeLists.copy.txt" NEW_HASH)
+
+    if (NOT "${OLD_HASH}" STREQUAL "${NEW_HASH}")
+      set(NEEDS_UPDATE TRUE)
+    endif()
+  endif()
+
+  # If the build script got updated then we need to build irregardless of
+  # whether the dependency has been installed in the local dir before.
+  if (NEEDS_UPDATE)
+    file(RENAME "${SUB_DIR}/CMakeLists.copy.txt" "${SUB_DIR}/CMakeLists.txt")
+  else()
+    find_package(${PACKAGE} ${VERSION} QUIET)
+    if (${PACKAGE}_FOUND)
+      return()
+    endif()
+  endif()
+
+  message(STATUS "Building dependency ${PACKAGE}")
+  execute_process(
+    COMMAND "${CMAKE_COMMAND}"
+      -B "${SUB_DIR}"
+      -S "${SUB_DIR}"
+      -G "${CMAKE_GENERATOR}"
+    RESULT_VARIABLE EXITCODE
   )
 
-  FetchContent_MakeAvailable(magic_enum)
+  if (NOT EXITCODE EQUAL 0)
+    message(FATAL_ERROR "Could not configure ${PACKAGE} super-project")
+  endif()
 
-  set(magic_enum_NO_PACKAGE_FOUND TRUE CACHE INTERNAL "")
-endif()
+  execute_process(
+    COMMAND "${CMAKE_COMMAND}" --build "${SUB_DIR}"
+    RESULT_VARIABLE EXITCODE
+  )
+
+  if (NOT EXITCODE EQUAL 0)
+    message(FATAL_ERROR "Failed to build ${PACKAGE}")
+  endif()
+
+  find_package(${PACKAGE} ${VERSION} REQUIRED)
+endfunction()
+
+caffeine_dependency(
+  magic_enum     0.7.2
+  GIT_REPOSITORY https://github.com/Neargye/magic_enum.git
+  GIT_TAG        v0.7.2
+  GIT_SHALLOW    TRUE
+  CMAKE_CACHE_ARGS
+    -DMAGIC_ENUM_OPT_BUILD_EXAMPLES:BOOL=FALSE
+    -DMAGIC_ENUM_OPT_BUILD_TESTS:BOOL=FALSE
+)
