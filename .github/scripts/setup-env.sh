eea98a47e60409fb961b81d84de7fdf9e274e23e
Phantomical <phantom@lynches.ca>
2021-04-16 23:25:23 -0400

    Use a script in CI to simplify the build matrix

---
+++ .github/scripts/setup-env.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This script is meant to simplify the compilation matrix in the case where we have
+# multiple dependent definitions
+
+function error {
+  echo "$1" >&2
+}
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --compiler)
+      COMPILER="$2"
+      ;;
+    *)
+      error "Unknown option '$1'"
+      exit 1
+      ;;
+  esac
+done
+
+case $COMPILER in
+  clang)
+    export CC=clang
+    export CXX=clang++
+    ;;
+  gcc)
+    export CC=gcc
+    export CXX=g++
+    ;;
+esac
