f4ee717d1c36e4da1aca9ede77114f69e4a86b5d
Sean Lynch <phantom@lynches.ca>
2021-04-28 22:56:48 -0400

    Implement more robust patching script (#363)

--- cmake/CaffeinePatch.cmake
+++ cmake/CaffeinePatch.cmake
@@ -8,6 +8,15 @@ if (NOT EXITCODE EQUAL 0)
   message (FATAL_ERROR "Failed to reset repo")
 endif()
 
+execute_process(
+  COMMAND git clean -fxd
+  RESULT_VARIABLE EXITCODE
+)
+
+if (NOT EXITCODE EQUAL 0)
+  message (FATAL_ERROR "Failed to clean repo")
+endif()
+
 set(ARG_NUM 1)
 while (ARG_NUM LESS CMAKE_ARGC)
   set(ARG "${CMAKE_ARGV${ARG_NUM}}")
