18f87e5fca943ca62faa7f6f6ead6d6352552282
Misha Zharov <mishazharov1@gmail.com>
2021-05-16 21:37:06 -0400

    Add optional extra Assertion to on_path_complete
    This PR adds an optional Assertion to ExecutionPolicy::on_path_complete
    which will allow us to simplify the error handling of Policies (making
    use of the logFailure handler unnecessary).

--- include/caffeine/Interpreter/Policy.h
+++ include/caffeine/Interpreter/Policy.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "caffeine/IR/Assertion.h"
+
 namespace caffeine {
 
 class Context;
@@ -52,7 +54,7 @@ public:
   //
   // Note that when the exit status is `Fail` the context will continue to be
   // used for the non-failure path if it is valid.
-  virtual void on_path_complete(const Context& ctx, ExitStatus status);
+  virtual void on_path_complete(const Context& ctx, ExitStatus status, const Assertion& assertion = Assertion());
 
 protected:
   ExecutionPolicy(ExecutionPolicy&&) = default;
