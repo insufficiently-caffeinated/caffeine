ac41fe25cf950d5f3b47df163902955e44acf6f7
Misha Zharov <mishazharov1@gmail.com>
2021-05-27 23:56:24 -0400

    Remove wrong comment

--- test/run-fail/xorfun.c
+++ test/run-fail/xorfun.c
@@ -1,7 +1,6 @@
 #include "caffeine.h"
 #include <stdint.h>
 
-// Test that signed division checks for divide-by-zero.
 void test(int32_t secret) {
   if ((secret & 0b1) == 1) {
     if ((secret & 0b10) == 0) {
