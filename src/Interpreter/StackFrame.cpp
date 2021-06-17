3bd0eacf574f920ce09b563fbad0d7a517a0a768
Phantomical <phantom@lynches.ca>
2021-06-17 18:24:06 -0400

    Add unique stack frame id marker

--- src/Interpreter/StackFrame.cpp
+++ src/Interpreter/StackFrame.cpp
@@ -8,9 +8,11 @@
 
 namespace caffeine {
 
+std::atomic<uint64_t> StackFrame::next_frame_id{0};
+
 StackFrame::StackFrame(llvm::Function* function)
     : current_block(&function->getEntryBlock()), prev_block(nullptr),
-      current(current_block->begin()) {}
+      current(current_block->begin()), frame_id(next_frame_id++) {}
 
 void StackFrame::jump_to(llvm::BasicBlock* block) {
   prev_block = current_block;
