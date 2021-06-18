521920be6232994d433b18fed0b8aaa1e65c9651
Sean Lynch <phantom@lynches.ca>
2021-06-18 15:31:59 -0400

    Fix assertion when loading a boolean from memory (#450)
    * Allow same-type trunc, zext, and sext
    
    These just get simplified to no-ops and it makes other code easier to
    write.
    
    * Only return raw bytes if output bitwidth is 8 bits
    
    Previously we'd just directly return the type if its size in bytes was
    1 byte, but this was incorrect for types which were not 8 bit integers
    (e.g. i1)
    
    * Add regression test for issue 362
    
    Closes #362
    
    * Move simplifications to proper location

--- src/IR/Operation.cpp
+++ src/IR/Operation.cpp
@@ -648,21 +648,21 @@ DECL_UNOP_CREATE(FIsNaN, ASSERT_FP, Type::int_ty(1));
 OpRef UnaryOp::CreateTrunc(Type tgt, const OpRef& operand) {
   CAFFEINE_ASSERT(tgt.is_int());
   CAFFEINE_ASSERT(operand->type().is_int());
-  CAFFEINE_ASSERT(tgt.bitwidth() < operand->type().bitwidth());
+  CAFFEINE_ASSERT(tgt.bitwidth() <= operand->type().bitwidth());
 
   return Create(Opcode::Trunc, operand, tgt);
 }
 OpRef UnaryOp::CreateZExt(Type tgt, const OpRef& operand) {
   CAFFEINE_ASSERT(tgt.is_int());
   CAFFEINE_ASSERT(operand->type().is_int());
-  CAFFEINE_ASSERT(tgt.bitwidth() > operand->type().bitwidth());
+  CAFFEINE_ASSERT(tgt.bitwidth() >= operand->type().bitwidth());
 
   return Create(Opcode::ZExt, operand, tgt);
 }
 OpRef UnaryOp::CreateSExt(Type tgt, const OpRef& operand) {
   CAFFEINE_ASSERT(tgt.is_int());
   CAFFEINE_ASSERT(operand->type().is_int());
-  CAFFEINE_ASSERT(tgt.bitwidth() > operand->type().bitwidth());
+  CAFFEINE_ASSERT(tgt.bitwidth() >= operand->type().bitwidth());
 
   return Create(Opcode::SExt, operand, tgt);
 }
