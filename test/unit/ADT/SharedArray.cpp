addc40df0725758a4a9e043f9a2fbff69052ce42
Brooke Dolny <brookedolny@gmail.com>
2021-06-19 16:09:41 -0400

    Shared array tests (#451)
    * Copy assignment and equality operator tests
    
    * Added move and modified tests
    
    MAO and multiple copies
    Made a larger modification to the large SharedArray
    Trying really hard to cover the move assignment operator
    
    * Added move operation
    
    * Fixed format error

--- test/unit/ADT/SharedArray.cpp
+++ test/unit/ADT/SharedArray.cpp
@@ -13,9 +13,11 @@ class SharedArrayTest : public ::testing::Test {
 protected:
   std::vector<char> zeros;
   std::vector<char> large;
+  std::vector<char> small;
 
   void SetUp() override {
     zeros = std::vector<char>(2 * SharedArray::min_copy_size, 0);
+    small = std::vector<char>(SharedArray::min_copy_size, 0);
 
     large.clear();
     large.reserve(16000);
@@ -60,4 +62,75 @@ TEST_F(SharedArrayTest, flattens_when_sufficiently_modified) {
     ASSERT_EQ(copy[i], 5);
 
   ASSERT_TRUE(copy.is_flat());
+}
+
+TEST_F(SharedArrayTest, copy_and_modify_small_shared_array) {
+  SharedArray array = small;
+
+  for (size_t i = 0; i < array.size(); ++i) {
+    array.store(i, (char)i);
+  }
+
+  SharedArray cpy = array;
+  ASSERT_TRUE(cpy == array);
+
+  cpy.store(0, 10);
+  ASSERT_TRUE(cpy != array);
+
+  SharedArray second_cpy = cpy;
+  ASSERT_TRUE(cpy == second_cpy);
+}
+
+TEST_F(SharedArrayTest, copy_and_modify_large_shared_array) {
+  SharedArray array = large;
+
+  for (size_t i = 0; i < array.size() / 2; ++i) {
+    array.store(i, i % 128);
+  }
+
+  SharedArray cpy = array;
+  ASSERT_TRUE(cpy == array);
+
+  for (size_t i = 0; i < array.size() / 2; ++i) {
+    cpy.store(i, (i + 10) % 128);
+  }
+  ASSERT_TRUE(cpy != array);
+
+  SharedArray second_cpy = cpy;
+  ASSERT_TRUE(cpy == second_cpy);
+}
+
+TEST_F(SharedArrayTest, move_and_modify_small_shared_array) {
+  SharedArray array(large);
+  {
+    SharedArray sm(small);
+    array = std::move(sm);
+  }
+  for (size_t i = 0; i < array.size(); ++i) {
+    ASSERT_TRUE(array[i] == small[i]);
+  }
+}
+
+TEST_F(SharedArrayTest, equality_operator_tests) {
+  SharedArray small_array = small;
+  SharedArray second_small_array = small;
+  SharedArray large_array = large;
+
+  ASSERT_TRUE(small_array == second_small_array);
+  ASSERT_FALSE(small_array == large_array);
+
+  second_small_array.store(0, 10);
+  ASSERT_FALSE(second_small_array == small_array);
+}
+
+TEST_F(SharedArrayTest, inequality_operator_tests) {
+  SharedArray small_array = small;
+  SharedArray second_small_array = small;
+  SharedArray large_array = large;
+
+  ASSERT_FALSE(small_array != second_small_array);
+  ASSERT_TRUE(small_array != large_array);
+
+  second_small_array.store(0, 10);
+  ASSERT_TRUE(second_small_array != small_array);
 }
