067c237059d5c03b3a4e017147bcf12725ac6dc0
Phantomical <phantom@lynches.ca>
2021-06-19 21:18:35 -0400

    Add -fno-rtti flags to be compatible with fno-rtti LLVM

--- cmake/CaffeineFlags.cmake
+++ cmake/CaffeineFlags.cmake
@@ -4,3 +4,9 @@
 add_compile_options(
   "$<$<AND:$<CXX_COMPILER_ID:GCC,Clang>,$<CONFIG:RelWithDebInfo>>:-fno-omit-frame-pointer>"
 )
+
+# Most distributions of LLVM are built without RTTI, so we need to set this to avoid
+# missing symbol errors.
+add_compile_options(
+  "$<$<CXX_COMPILER_ID:GCC,Clang>:-fno-rtti>"
+)
