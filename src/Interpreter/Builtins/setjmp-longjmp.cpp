407705518669fdc399f07c2ea05b3bf5110041e8
Phantomical <phantom@lynches.ca>
2021-06-21 17:06:34 -0400

    Add explanation comment

--- src/Interpreter/Builtins/setjmp-longjmp.cpp
+++ src/Interpreter/Builtins/setjmp-longjmp.cpp
@@ -3,6 +3,42 @@
 #include <fmt/format.h>
 
 namespace caffeine {
+
+/**
+ * Implementing setjmp and longjmp
+ * ===============================
+ * To implement setjmp and longjmp on a normal architecture we need to somehow
+ * record the current location of the thread and then return to it at a later
+ * time (when longjmp is called). This is done by recording the registers at
+ * that and then restoring them (mostly) when longjmp is called which has the
+ * effect of returning to the call location.
+ *
+ * To implement these within caffeine we need to do much of the same thing.
+ * Caffeine doesn't have any registers so all we really need to do is restore
+ * the stack frame and the instruction pointer at the time setjmp was called.
+ * The way that this is currently done is by saving two bits of info within the
+ * jump buffer type:
+ *
+ * 1. The frame ID of the frame that first called setjmp. This is the frame we
+ *    need to return to when longjmp is called.
+ * 2. The llvm instruction pointer that corresponds to the call instruction to
+ *    setjmp. This is the instruction we'll need to return to when longjmp is
+ *    called.
+ *
+ * Then, when longjmp is called we search for the correct frame and instruction
+ * pointer that we read from the interpreted program's memory. If we find them
+ * then we drop all stack frames below the initial setjmp, reset the instruction
+ * pointer, and set up the return value.
+ *
+ * Downsides
+ * =========
+ * This implementation has the problem that we are storing host-specific
+ * pointers within the memory of the program being interpreted. At the moment,
+ * this does not cause any issues. However, as soon as we want to go distributed
+ * it's going to have to be changed to something that remains the same across
+ * hosts.
+ */
+
 namespace {
   using ContextState = TransformBuilder::ContextState;
   using InsertFn = TransformBuilder::InsertFn;
@@ -181,7 +217,16 @@ ExecutionResult Interpreter::visitLongjmp(llvm::CallInst& inst) {
 
     state.ctx.stack.erase(state.ctx.stack.begin() + *target_frame,
                           state.ctx.stack.end());
-    state.ctx.stack_top().insert(jmp_tgt, state.lookup(inst.getArgOperand(1)));
+    auto& frame = state.ctx.stack_top();
+    frame.insert(jmp_tgt, state.lookup(inst.getArgOperand(1)));
+    frame.jump_to(jmp_tgt->getParent());
+
+    while (&*frame.current != jmp_tgt) {
+      ++frame.current;
+      CAFFEINE_ASSERT(frame.current != frame.current_block->end());
+    }
+    // Don't want to execute _setjmp again
+    ++frame.current;
 
     insert_fn(std::move(state));
   });
