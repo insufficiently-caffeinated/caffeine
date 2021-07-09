8a21a011d0f81bfcbf5b850cf44a670b01ed1b3c
Phantomical <phantom@lynches.ca>
2021-07-09 17:51:26 -0400

    Allow ThreadMap to work with non-default-constructible types

--- include/caffeine/ADT/ThreadMap.h
+++ include/caffeine/ADT/ThreadMap.h
@@ -16,7 +16,7 @@ public:
     auto lock = std::shared_lock(mutex);
 
     auto it = map.find(id);
-    return it == map.end() ? nullptr : &it->second;
+    return it == map.end() ? nullptr : it->second.get();
   }
   T* get() {
     return const_cast<T*>(const_cast<const ThreadMap<T>*>(this)->get());
@@ -26,18 +26,25 @@ public:
     return get_or_insert();
   }
 
-  T& get_or_insert() {
+  template <typename... Args>
+  T& get_or_insert(Args&&... args) {
     if (auto* ptr = get())
       return *ptr;
 
     auto id = std::this_thread::get_id();
     auto lock = std::unique_lock(mutex);
-    return map[id];
+    auto it = map.find(id);
+    if (it == map.end()) {
+      it = map.emplace(id, std::make_unique<T>(std::forward<Args>(args)...))
+               .first;
+    }
+
+    return *it->second;
   }
 
 private:
   mutable std::shared_mutex mutex;
-  std::unordered_map<std::thread::id, T> map;
+  std::unordered_map<std::thread::id, std::unique_ptr<T>> map;
 };
 
 } // namespace caffeine
