4b507e823944ea3a894833b228598b3c7b161370
Phantomical <phantom@lynches.ca>
2021-07-11 16:54:17 -0400

    Add github actions workflow with caching (#468)
    This commit introduces a new github actions workflow that avoids
    downloading the large docker image along with a number of other build
    performance improvements.
    
    Summary of improvements:
    - Just use apt-get to install dependencies since many of them are
      already installed within the ubuntu-latest GH actions image.
    - Use ninja to build in CI for better progress messages.
    - Cache the output directory (per-compiler) so that it can be reused for
      later compilation steps.
    
    When there are cached build artifacts this is able to run in ~1 minute
    whereas before it took ~12 minutes for the GCC build.

---
+++ .github/scripts/update-alternatives-llvm.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+
+# Use update-alternatives to register all llvm utility versions
+
+version=$1
+priority=$2
+
+update-alternatives \
+    --verbose \
+    --install /usr/bin/llvm-config       llvm-config      /usr/bin/llvm-config-${version} ${priority} \
+    --slave   /usr/bin/llvm-ar           llvm-ar          /usr/bin/llvm-ar-${version} \
+    --slave   /usr/bin/llvm-as           llvm-as          /usr/bin/llvm-as-${version} \
+    --slave   /usr/bin/llvm-bcanalyzer   llvm-bcanalyzer  /usr/bin/llvm-bcanalyzer-${version} \
+    --slave   /usr/bin/llvm-cov          llvm-cov         /usr/bin/llvm-cov-${version} \
+    --slave   /usr/bin/llvm-diff         llvm-diff        /usr/bin/llvm-diff-${version} \
+    --slave   /usr/bin/llvm-dis          llvm-dis         /usr/bin/llvm-dis-${version} \
+    --slave   /usr/bin/llvm-dwarfdump    llvm-dwarfdump   /usr/bin/llvm-dwarfdump-${version} \
+    --slave   /usr/bin/llvm-extract      llvm-extract     /usr/bin/llvm-extract-${version} \
+    --slave   /usr/bin/llvm-link         llvm-link        /usr/bin/llvm-link-${version} \
+    --slave   /usr/bin/llvm-mc           llvm-mc          /usr/bin/llvm-mc-${version} \
+    --slave   /usr/bin/llvm-nm           llvm-nm          /usr/bin/llvm-nm-${version} \
+    --slave   /usr/bin/llvm-objdump      llvm-objdump     /usr/bin/llvm-objdump-${version} \
+    --slave   /usr/bin/llvm-ranlib       llvm-ranlib      /usr/bin/llvm-ranlib-${version} \
+    --slave   /usr/bin/llvm-readobj      llvm-readobj     /usr/bin/llvm-readobj-${version} \
+    --slave   /usr/bin/llvm-rtdyld       llvm-rtdyld      /usr/bin/llvm-rtdyld-${version} \
+    --slave   /usr/bin/llvm-size         llvm-size        /usr/bin/llvm-size-${version} \
+    --slave   /usr/bin/llvm-stress       llvm-stress      /usr/bin/llvm-stress-${version} \
+    --slave   /usr/bin/llvm-symbolizer   llvm-symbolizer  /usr/bin/llvm-symbolizer-${version} \
+    --slave   /usr/bin/llvm-tblgen       llvm-tblgen      /usr/bin/llvm-tblgen-${version} \
+    --slave   /usr/bin/llvm-objcopy      llvm-objcopy     /usr/bin/llvm-objcopy-${version} \
+    --slave   /usr/bin/llvm-strip        llvm-strip       /usr/bin/llvm-strip-${version}
+
+update-alternatives \
+    --verbose \
+    --install /usr/bin/clang             clang            /usr/bin/clang-${version} ${priority} \
+    --slave   /usr/bin/asan_symbolize    asan_symbolize   /usr/bin/asan_symbolize-${version} \
+    --slave   /usr/bin/clang-cpp         clang-cpp        /usr/bin/clang-cpp-${version} \
+    --slave   /usr/bin/ld.lld            ld.lld           /usr/bin/ld.lld-${version}
+
+update-alternatives \
+  --verbose \
+  --install   /usr/bin/clang++           clang++          /usr/bin/clang++-${version} ${priority}
