a7fd9302c86ba5b355e172edabecfd28a1f3ccce
Phantomical <phantom@lynches.ca>
2021-07-12 18:02:36 -0400

    Reimplement OperationCache to be more performant
    The new version is both public and should be faster in the case where
    the elements are already within the cache.

--- include/caffeine/IR/OperationCache.h
+++ include/caffeine/IR/OperationCache.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "caffeine/IR/Operation.h"
-#include <boost/thread/shared_mutex.hpp>
+#include <shared_mutex>
 #include <tsl/hopscotch_set.h>
 
 namespace caffeine {
@@ -15,21 +15,60 @@ private:
   };
 
   struct equal_to {
+    using is_transparent = void;
+
     bool operator()(const OpRef& a, const OpRef& b) const {
       return *a == *b;
     }
+
+    bool operator()(const OpRef& a, const Operation& b) const {
+      return *a == b;
+    }
+    bool operator()(const Operation& a, const OpRef& b) const {
+      return a == *b;
+    }
   };
 
-  boost::upgrade_mutex mutex;
-  tsl::hopscotch_set<OpRef, hasher, equal_to, std::allocator<OpRef>, 30, true>
-      cache;
+  using set_type = tsl::hopscotch_set<OpRef, hasher, equal_to>;
+
+  // Minimum threshold for a GC collection to run automatically.
+  static constexpr size_t min_threshold = 2048;
+
+  mutable std::shared_mutex mutex;
+  set_type set;
+  size_t threshold = min_threshold;
 
 public:
   OperationCache() = default;
 
+  // Get a pointer to the default global cache.
+  static OperationCache* default_cache();
+
+  size_t size() const;
+
+  // Remove all items from the cache.
+  void clear();
+
   // Look up an opertion in the cache, inserting it if it is not already
   // present.
+  //
+  // Note that this method does not cache any of the operands of op. If you want
+  // to cache recursively then use intern instead.
   OpRef cache(Operation&& op);
+  OpRef cache(const Operation& op);
+
+  // Cache op and all of its operands recursively until everything is cached.
+  OpRef intern(const OpRef& op);
+
+  // Remove all items in the map with no external references.
+  //
+  // Normally the cache will automatically perform garbage collection every so
+  // often but this method can be useful for forcing it.
+  void gc();
+
+private:
+  void gc_locked(std::unique_lock<std::shared_mutex>&);
+  OpRef intern_locked(const OpRef& op, std::unique_lock<std::shared_mutex>&);
 };
 
 } // namespace caffeine
