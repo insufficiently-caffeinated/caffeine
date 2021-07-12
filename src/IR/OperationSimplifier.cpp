e34e549bedc06bcc52a981739695f5abb3b94d67
Phantomical <phantom@lynches.ca>
2021-07-12 19:46:40 -0400

    Move ConstantFolder to its own header

---
+++ src/IR/OperationSimplifier.cpp
@@ -0,0 +1,8 @@
+#include "caffeine/IR/OperationSimplifier.h"
+
+namespace caffeine {
+
+template class OperationSimplifier<true>;
+template class OperationSimplifier<false>;
+
+} // namespace caffeine
