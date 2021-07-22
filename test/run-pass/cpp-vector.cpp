ff45e38b541f03ae5e4637d7689a08a8d8a0e813
Misha Zharov <mishazharov1@gmail.com>
2021-07-21 21:32:17 -0400

    Delete test that uses unimplemented things

--- test/run-pass/cpp-vector.cpp
+++
@@ -1,9 +0,0 @@
-#include "caffeine.h"
-#include <cstdint>
-#include <vector>
-
-extern "C" void test(int32_t x) {
-  std::vector<int32_t> vec;
-  vec.push_back(x);
-  caffeine_assert(vec.at(0) == x);
-}
