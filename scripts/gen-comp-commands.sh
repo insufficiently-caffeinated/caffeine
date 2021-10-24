cf7fceb872e2a130779a1d5653d84190d895e810
Phantomical <phantom@lynches.ca>
2021-10-23 17:15:27 -0700

    Format comp-commands script

--- scripts/gen-comp-commands.sh
+++ scripts/gen-comp-commands.sh
@@ -4,4 +4,6 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
 
 bazel build //tools/compdb
-./bazel-bin/tools/compdb/compdb 'deps(//... union @llvm//llvm:all union @llvm//clang:all)' > compile_commands.json
+./bazel-bin/tools/compdb/compdb \
+    'deps(//... union @llvm//llvm:all union @llvm//clang:all)' \
+    > compile_commands.json
