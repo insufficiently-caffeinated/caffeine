8317ddb4b22302c93a43a5eb04989cb7d28f16ba
Phantomical <phantom@lynches.ca>
2021-10-25 01:08:00 -0700

    Add builtin definitions for memcpy, memset, amd memmove

---
+++ libraries/builtins/string.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+void* memcpy(void* __restrict dest, const void* __restrict src, size_t size) {
+  return __builtin_memcpy(dest, src, size);
+}
+
+void* memmove(void* dest, const void* src, size_t size) {
+  return __builtin_memmove(dest, src, size);
+}
+
+void* memset(void* mem, int val, size_t size) {
+  return __builtin_memset(mem, val, size);
+}
