7647435fa00b9a2f3dc5863a576374821f05f7c2
Phantomical <phantom@lynches.ca>
2021-10-27 23:58:26 -0700

    Silence bazel logs unless an error occurs in output-path.sh

--- scripts/output-path.sh
+++ scripts/output-path.sh
@@ -6,4 +6,10 @@ shift
 bazel cquery \
   --output=starlark \
   --starlark:expr="target.files.to_list()[0].path" \
-  "'$TARGET'" "$@"
+  "'$TARGET'" "$@" 2> bazel-out/query.log
+
+if [ $? -ne 0 ]; then
+  cat bazel-out/query.log 1>&2
+fi
+
+rm bazel-out/query.log
