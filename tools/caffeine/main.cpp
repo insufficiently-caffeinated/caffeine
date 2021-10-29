bcc971880a784e2d45b5f05c8ce299d6ced54635
Phantomical <phantom@lynches.ca>
2021-10-28 20:42:16 -0700

    Don't include LoggingSolver by default

--- tools/caffeine/main.cpp
+++ tools/caffeine/main.cpp
@@ -161,7 +161,6 @@ int main(int argc, char** argv) {
 
   auto policy = caffeine::AlwaysAllowExecutionPolicy();
   auto builder = caffeine::SolverBuilder::with_default();
-  builder.with<LoggingSolver>();
   
   auto exec =
       caffeine::Executor(&policy, store.get(), &logger, &builder, options);
