fa5421486d1c3ee7daf703f602c585341b3c2bc0
Phantomical <phantom@lynches.ca>
2021-12-19 01:52:15 -0500

    Delete old compdb generation tools

--- scripts/gen-comp-commands.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "$( dirname "${BASH_SOURCE[0]}" )/.."
-
-
-bazel build //tools/compdb
-./bazel-bin/tools/compdb/compdb \
-    'deps(//... union @llvm//llvm:all union @llvm//clang:all)' \
-    > compile_commands.json
