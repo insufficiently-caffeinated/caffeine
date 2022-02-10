5eff3dc49464357a3bc3863cfcde40bc26ceba12
Phantomical <phantom@lynches.ca>
2022-02-10 23:58:24 +0000

    Add commutativity rules

--- src/IR/EMatching/AllMatchers.cpp
+++ src/IR/EMatching/AllMatchers.cpp
@@ -17,6 +17,26 @@ void EMatcherBuilder::add_defaults() {
                   egraph.get(eclass)->type().bitwidth()))});
         });
   }
+
+  { // (#binop ?x ?y) -> (#binop ?y ?x) for commutative operations
+    Operation::Opcode valid[] = {
+        Operation::Add,    Operation::Mul,    Operation::And,
+        Operation::Or,     Operation::Xor,    Operation::FAdd,
+        Operation::FSub,   Operation::FMul,   Operation::ICmpEq,
+        Operation::ICmpNe, Operation::FCmpEq, Operation::FCmpNe};
+
+    for (auto opcode : valid) {
+      size_t clause = add_clause(opcode);
+      add_matcher(clause, [](const MatchData&, EGraph& egraph, size_t eclass_id,
+                             size_t node_id) {
+        const EClass* eclass = egraph.get(eclass_id);
+        const ENode& node = eclass->nodes[node_id];
+
+        egraph.add_merge(
+            eclass_id, ENode{node.data, {node.operands[1], node.operands[0]}});
+      });
+    }
+  }
 }
 
 } // namespace caffeine::ematching
--- test/unit/IR/EMatching.cpp
+++ test/unit/IR/EMatching.cpp
@@ -76,3 +76,20 @@ TEST_F(EMatchingTests, sub_elimination) {
 
   ASSERT_EQ(egraph.find(cid), egraph.find(did));
 }
+
+TEST_F(EMatchingTests, commutativity) {
+  auto a = add(Constant::Create(Type::int_ty(32), "a"));
+  auto b = add(Constant::Create(Type::int_ty(32), "b"));
+  auto c = add(BinaryOp::CreateAdd(a, b));
+  auto d = add(BinaryOp::CreateAdd(b, a));
+
+  size_t aid = egraph.add(*a);
+  size_t bid = egraph.add(*b);
+  size_t cid = egraph.add(*c);
+  size_t did = egraph.add(*d);
+
+  egraph.simplify(matcher);
+
+  ASSERT_EQ(egraph.find(cid), egraph.find(did));
+  ASSERT_NE(egraph.find(aid), egraph.find(bid));
+}
