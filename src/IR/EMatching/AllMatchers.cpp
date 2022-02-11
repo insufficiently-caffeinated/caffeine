5440597136543d6e22eda77d9f5121d4f42bd421
Phantomical <phantom@lynches.ca>
2022-02-11 00:41:14 +0000

    Refactor associativity reduction to a separate method

--- src/IR/EMatching/AllMatchers.cpp
+++ src/IR/EMatching/AllMatchers.cpp
@@ -5,6 +5,37 @@
 namespace caffeine::ematching::reductions {
 
 
+void associativity(EMatcherBuilder& builder) {
+  Operation::Opcode valid[] = {Operation::Add,  Operation::Mul, Operation::And,
+                               Operation::Or,   Operation::Xor, Operation::FAdd,
+                               Operation::FSub, Operation::FMul};
+
+  for (auto opcode : valid)
+    associativity(builder, opcode);
+}
+void associativity(EMatcherBuilder& builder, Operation::Opcode opcode) {
+  size_t any = builder.add_clause(Operation::Invalid);
+  size_t subclause = builder.add_clause(opcode);
+  size_t parent = builder.add_clause(opcode, {any, subclause});
+
+  builder.add_matcher(parent, [=](const MatchData& data, EGraph& egraph,
+                                  size_t eclass_id, size_t node_id) {
+    const EClass* parent = egraph.get(eclass_id);
+    const ENode& pnode = parent->nodes[node_id];
+
+    const EClass* child = egraph.get(pnode.operands[1]);
+    for (size_t cnode_id : data.matches(subclause, pnode.operands[1])) {
+      const ENode& cnode = child->nodes[cnode_id];
+
+      size_t child =
+          egraph.add(ENode{cnode.data, {pnode.operands[0], cnode.operands[0]}});
+
+      egraph.add_merge(eclass_id,
+                       ENode{pnode.data, {child, cnode.operands[1]}});
+    }
+  });
+}
+
 } // namespace caffeine::ematching::reductions
 
 namespace caffeine::ematching {
@@ -24,35 +55,7 @@ void EMatcherBuilder::add_defaults() {
   }
 
   reductions::commutativity(*this);
-
-  { // associativity: (#op ?x (#op ?y ?z)) -> (#op (#op ?x ?y) ?z)
-    Operation::Opcode valid[] = {
-        Operation::Add, Operation::Mul,  Operation::And,  Operation::Or,
-        Operation::Xor, Operation::FAdd, Operation::FSub, Operation::FMul};
-
-    for (auto opcode : valid) {
-      size_t any = add_clause(Operation::Invalid);
-      size_t subclause = add_clause(opcode);
-      size_t parent = add_clause(opcode, {any, subclause});
-
-      add_matcher(parent, [=](const MatchData& data, EGraph& egraph,
-                              size_t eclass_id, size_t node_id) {
-        const EClass* parent = egraph.get(eclass_id);
-        const ENode& pnode = parent->nodes[node_id];
-
-        const EClass* child = egraph.get(pnode.operands[1]);
-        for (size_t cnode_id : data.matches(subclause, pnode.operands[1])) {
-          const ENode& cnode = child->nodes[cnode_id];
-
-          size_t child = egraph.add(
-              ENode{cnode.data, {pnode.operands[0], cnode.operands[0]}});
-
-          egraph.add_merge(eclass_id,
-                           ENode{pnode.data, {child, cnode.operands[1]}});
-        }
-      });
-    }
-  }
+  reductions::associativity(*this);
 }
 
 } // namespace caffeine::ematching
