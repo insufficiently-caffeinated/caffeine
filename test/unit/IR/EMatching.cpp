8f03fab8014406aa597b5e8c5f0a22ef962a360d
Phantomical <phantom@lynches.ca>
2022-02-15 07:15:42 +0000

    Add a method to EGraphExtractor to update cached node costs

--- test/unit/IR/EMatching.cpp
+++ test/unit/IR/EMatching.cpp
@@ -37,3 +37,22 @@ TEST_F(EMatchingTests, commutativity) {
   ASSERT_EQ(egraph.find(cid), egraph.find(did));
   ASSERT_NE(egraph.find(aid), egraph.find(bid));
 }
+
+TEST_F(EMatchingTests, associativity) {
+  r::associativity(builder, Operation::Add);
+  auto matcher = builder.build();
+
+  auto a = add(Constant::Create(Type::int_ty(32), "a"));
+  auto b = add(Constant::Create(Type::int_ty(32), "b"));
+  auto c = add(Constant::Create(Type::int_ty(32), "c"));
+
+  auto t1 = add(BinaryOp::CreateAdd(a, BinaryOp::CreateAdd(b, c)));
+  auto t2 = add(BinaryOp::CreateAdd(BinaryOp::CreateAdd(a, b), c));
+
+  size_t id1 = egraph.add(*t1);
+  size_t id2 = egraph.add(*t2);
+
+  egraph.simplify(matcher);
+
+  ASSERT_EQ(egraph.find(id1), egraph.find(id2));
+}
