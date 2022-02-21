fc250d716149f060ac69196264b650c86ce69fc9
Misha Zharov <mishazharov1@gmail.com>
2022-02-21 16:34:02 -0500

    Fix TypeId lookup
    Stripping bitcasts makes sure we register the global object instead of
    some bitcast

--- src/Interpreter/TypeidDb.cpp
+++ src/Interpreter/TypeidDb.cpp
@@ -1,16 +1,24 @@
 #include <caffeine/Interpreter/TypeidDb.h>
+#include <caffeine/Support/Assert.h>
+
+#include "caffeine/Support/LLVMFmt.h"
+#include <fmt/format.h>
+#include <iostream>
 
 namespace caffeine {
 
 int32_t TypeidDb::get_selector(const llvm::Constant* exception_ty) {
+  CAFFEINE_ASSERT(exception_ty, "must not be a null pointer");
   std::unique_lock lock(mutex);
 
-  auto val = values.find(exception_ty);
-  if (val != values.end()) {
-    return val->second;
+  {
+    auto val = values.find(exception_ty->stripPointerCasts());
+    if (val != values.end()) {
+      return val->second;
+    }
   }
 
-  values[exception_ty] = curr;
+  values[exception_ty->stripPointerCasts()] = curr;
   int32_t res = curr;
   curr++;
   return res;
