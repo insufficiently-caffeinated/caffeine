63125b34b8d792942d002882639c38f19422b78a
Misha Zharov <mishazharov1@gmail.com>
2022-02-22 11:50:15 -0500

    Address PR feedback

--- include/caffeine/Interpreter/ExternalFuncs/GenericUnwinding.h
+++ include/caffeine/Interpreter/ExternalFuncs/GenericUnwinding.h
@@ -1,16 +1,4 @@
-#include "caffeine/Interpreter/ExternalFunction.h"
 #include "caffeine/Interpreter/InterpreterContext.h"
-#include "caffeine/Support/LLVMFmt.h"
-#include <boost/range/algorithm/find_if.hpp>
-#include <fmt/format.h>
-#include <iterator>
-#include <llvm/IR/InstrTypes.h>
-#include <llvm/IR/Instructions.h>
-#include <llvm/IR/Module.h>
-
-#include <iostream>
-
-#include "unwind.h"
 
 namespace caffeine {
 
