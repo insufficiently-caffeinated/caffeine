30b18a12559d940c72d74b19a52f2045ceded80a
Sean Lynch <phantom@lynches.ca>
2022-03-03 12:56:29 -0500

    Add a demo application for the poster (#733)

---
+++ bench/demo.c
@@ -0,0 +1,19 @@
+#include "caffeine.h"
+#include <string.h>
+#include <stdint.h>
+
+// Parse an integer from a string, skipping any initial spaces.
+uint32_t parse_int(const char* s) {
+  while (*s == ' ') s++;
+
+  uint32_t value = *s++ - '0';
+  while (*s++) value = 10 * value + (*s - '0');
+  return value;
+}
+
+int main(int argc, char** argv) {
+  char data[24];
+  caffeine_make_symbolic(data, sizeof(data), "data");
+  caffeine_assume(data[sizeof(data) - 1] == '\0');
+  return parse_int(data);
+}
